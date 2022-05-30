a90e714a7f21736ceb74b6754699390166c1218d
Lenovo <irinakokinova@gmail.com>
2022-05-30 18:52:08 -0500

    String method

---
+++ src/da1_intro/Intro.java
@@ -0,0 +1,12 @@
+package da1_intro;
+
+public class Intro {
+    public static void main(String[] args) {
+
+
+        //syntax String  any method
+        String srs = "Java";
+        System.out.println("srs.toLowerCase() = " + srs.toLowerCase());
+
+    }
+}
